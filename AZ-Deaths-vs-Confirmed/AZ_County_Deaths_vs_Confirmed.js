7fd56d2593bdc234fc2a444d13f7a1a0f7d49b5e
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-05-01 16:13:26 -0700

    Fixed the frame issues and added Deaths-vs-Confirmed Plot

--- AZ-Deaths-vs-Confirmed/AZ_County_Deaths_vs_Confirmed.js
+++ AZ-Deaths-vs-Confirmed/AZ_County_Deaths_vs_Confirmed.js
@@ -6,7 +6,7 @@ var xticks = [];
 var yticks = [];
 var frames = [];
 var sliderSteps = [];
-var config = {responsive: true}
+var config = {responsive: true};
 
 var xmax = 0;
 var ymax = 0;
@@ -14,7 +14,7 @@ var ymax = 0;
 var traces;
 
 function initData() {
-    Plotly.d3.csv("Arizona_county_historic_data.csv", function(err, data){
+    Plotly.d3.csv("../Arizona_county_historic_data.csv", function(err, data){
         initialData = data;
         processData();
     });
@@ -29,7 +29,7 @@ function processData(data){
         }
         dates = [... new Set(processedData.map(e => e['Date']))];
         xmax = Math.max(xmax, parseFloat(e['Confirmed']));
-        ymax = Math.max(ymax, parseFloat(e['New_Cases_Weekly']));
+        ymax = Math.max(ymax, parseFloat(e['Deaths']));
     });
     for(let x_range = 1; x_range <= xmax; x_range++){
         xticks.push(x_range);
@@ -70,7 +70,7 @@ function processData(data){
                 let trace = {};
                 let countyData = getByCounty(processedData, county);
                 trace['x'] = unpack(countyData, 'Confirmed');
-                trace['y'] = unpack(countyData, 'New_Cases_Weekly');
+                trace['y'] = unpack(countyData, 'Deaths');
                 trace['text'] = dates;
                 trace['mode'] = 'lines+markers';
                 trace['name'] = county;
@@ -78,7 +78,7 @@ function processData(data){
                 trace['customdata'] = countyData.map(e => e['County']);
                 trace['hovertemplate'] = '<b>%{customdata}</b>'+
                     '<br><b>Date: </b> %{text}'+
-                    '<br><b>New Cases (7-day rolling average):</b> %{y:.2f}'+
+                    '<br><b>Deaths:</b> %{y}'+
                     '<br><b>Total Cases:</b> %{x}'+
                     '<extra></extra>';
                 traces.push(trace);
@@ -95,17 +95,17 @@ function processData(data){
         let counties = [... new Set(processedData.map(e => e['County']))];
         let data = [];
         for(let county of counties){
-            if(county != "Unknown"){
-                let trace = {x:[], y:[]};
+            if(county != "Unknown") {
+                let trace = {x: [], y: []};
                 let countyData = getByCounty(processedData, county);
                 let index = dates.indexOf(date);
-                console.log(index);
                 let confirmed = unpack(countyData, 'Confirmed');
-                let new_cases_weekly = unpack(countyData, 'New_Cases_Weekly');
+                let deaths = unpack(countyData, 'Deaths');
                 // let frameData = countyData.filter(e => e['Date'] == date);
-                trace.x = confirmed.slice(0, index+1);
-                trace.y = new_cases_weekly.slice(0, index+1);
+                trace.x = confirmed.slice(0, index + 1);
+                trace.y = deaths.slice(0, index + 1);
                 trace.id = county;
+
                 data.push(trace);
             }
         }
@@ -141,90 +141,63 @@ function processData(data){
 
 
 function plot(){
-    var myPlot = document.getElementById('myPlot'),
-    hoverInfo = document.getElementById('hoverinfo'),
-    layout = {
-        // autosize: false,
-        hovermode: 'closest',
-        height: 750,
-        title:'COVID-19: New Cases vs Total Cases - Logarithmic Scale',
-        xaxis:{
-            title: "Total Cases",
-            type: 'log',
-            dtick: 1
-            // tickvals: xticks
-        },
-        yaxis:{
-            title: "New Cases Weekly , 7-day average",
-            type: 'log',
-            tick0: 1,
-            // tickmode: "linear",
-            dtick: 1,
-            // tickvals: yticks
-        },
-        updatemenus: [{
-            x: 0,
-            y: 0,
-            yanchor: 'top',
-            xanchor: 'left',
-            showactive: false,
-            direction: 'left',
-            type: 'buttons',
-            pad: {t: 87, r: 10},
-            buttons: [{
-                method: 'animate',
-                args: [null, {
-                    mode: 'immediate',
-                    fromcurrent: true,
-                    transition: {duration: 0},
-                    frame: {duration: 300, redraw: true}
-                }],
-                label: 'Play'
-            }, {
-                method: 'animate',
-                args: [[null], {
-                    mode: 'immediate',
-                    transition: {duration: 0},
-                    frame: {duration: 0, redraw: false}
-                }],
-                label: 'Pause'
-            }]
-        }],
-
-        sliders: [{
-            pad: {l: 130, t: 55},
-            currentvalue: {
-                visible: true,
-                prefix: 'Date:',
-                xanchor: 'right',
-                font: {size: 20, color: '#666'}
+    var layout = {
+            // autosize: false,
+            hovermode: 'closest',
+            height: 750,
+            title:'COVID-19: Deaths vs Cases',
+            xaxis:{
+                title: "Number of Total Cases",
             },
-            steps: sliderSteps
-        }]
-    };
+            yaxis:{
+                title: "Number of Total Deaths",
+            },
+            updatemenus: [{
+                x: 0,
+                y: 0,
+                yanchor: 'top',
+                xanchor: 'left',
+                showactive: false,
+                direction: 'left',
+                type: 'buttons',
+                pad: {t: 87, r: 10},
+                buttons: [{
+                    method: 'animate',
+                    args: [null, {
+                        mode: 'immediate',
+                        fromcurrent: true,
+                        transition: {duration: 0},
+                        frame: {duration: 300, redraw: true}
+                    }],
+                    label: 'Play'
+                }, {
+                    method: 'animate',
+                    args: [[null], {
+                        mode: 'immediate',
+                        transition: {duration: 0},
+                        frame: {duration: 0, redraw: false}
+                    }],
+                    label: 'Pause'
+                }]
+            }],
+
+            sliders: [{
+                pad: {l: 130, t: 55},
+                currentvalue: {
+                    visible: true,
+                    prefix: 'Date:',
+                    xanchor: 'right',
+                    font: {size: 20, color: '#666'}
+                },
+                steps: sliderSteps
+            }]
+        };
     Plotly.newPlot('myPlot',{
         data: traces,
         layout: layout,
         frames: frames,
         config: config
     });
-
-    // myPlot.on('plotly_hover', function(data){
-    //     var infotext = data.points.map(function(d){
-    //         debugger;
-    //         return (
-    //             '<b>' + d.data.name + '<b>' +
-    //                 '<br>'+ '<b>Date:</b>' + d.text + '<br>' +
-    //                 '<br>' + '<b>New Cases (7-day rolling average):</b>' + d.y + '<br>' +
-    //                 '<br>' + '<b>Total Cases:</b>' + d.x + '<br>'
-    //         );
-    //     });
-    //     hoverInfo.innerHTML = infotext.join('<br/>');
-    // });
-    //
-    // myPlot.on('plotly_unhover', function(data){
-    //     hoverInfo.innerHTML = '';
-    // });
 }
 
 
